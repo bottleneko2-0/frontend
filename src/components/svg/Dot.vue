ce0cf81e8786b76b394cf74ff84faa932bac9d96
DESKTOP-EJEGLOR\MEA <graywolf7235@gmail.com>
2025-01-07 00:30:22 +0800

    引入svg元件 調整RWD跑版問題 新增幾個svg元件

---
+++ src/components/svg/Dot.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Base from '@/components/svg/Base.vue'
+</script>
+
+<template>
+  <Base v-bind="$attrs">
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z"
+    ></path>
+  </Base>
+</template>
